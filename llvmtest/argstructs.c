d110f921dd8ca89b1756e1a1f6300cb3e75d43a2
sheijk <sheijk@38bfec48-a720-0410-a12a-c49042e68019>
2008-01-24 04:31:47 +0000

    - 4 sh basis functions are stored into one texture
    git-svn-id: http://subversion.dword.org/repos/sheijk/trunk/Stuff/ocaml/lang/v3@424 38bfec48-a720-0410-a12a-c49042e68019

---
+++ llvmtest/argstructs.c
@@ -0,0 +1,30 @@
+
+
+typedef struct {
+  float x;
+  float y;
+  float z;
+} Point;
+
+void putPoint( Point p )
+{
+}
+
+Point getPoint()
+{
+  Point p;
+  return p;
+}
+
+int main()
+{
+  Point p;
+  
+/*   putPoint( p ); */
+
+  p = getPoint();
+
+  return 100;
+}
+
+
