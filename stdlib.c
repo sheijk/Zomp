785d7fdc986ab3a9c1c3fc1af186287766b84151
sheijk <sheijk@38bfec48-a720-0410-a12a-c49042e68019>
2009-11-22 23:35:17 +0000

    put function to load file to memory until we support C's io functions or have a custom library
    git-svn-id: http://subversion.dword.org/repos/sheijk/trunk/Stuff/ocaml/lang/v3@1062 38bfec48-a720-0410-a12a-c49042e68019

--- stdlib.c
+++ stdlib.c
@@ -90,6 +90,30 @@ bool zompCheckNativeSymbol(const char* name) {
   return dlsym( NULL, name ) != NULL;
 }
 
+// ripped from http://www.anyexample.com/programming/c/how_to_load_file_into_memory_using_plain_ansi_c_language.xml
+int zompLoadFileToMemory(const char *filename, char **result)
+{
+    int size = 0;
+    FILE *f = fopen(filename, "rb");
+    if (f == NULL) 
+    {
+        *result = NULL;
+        return -1; // -1 means file opening fail
+    }
+    fseek(f, 0, SEEK_END);
+    size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    *result = (char *)malloc(size+1);
+    if (size != fread(*result, sizeof(char), size, f))
+    {
+        free(*result);
+        return -2; // -2 means file reading fail
+    }
+    fclose(f);
+    (*result)[size] = 0;
+    return size;
+}
+
 /* NSBundle* webKitBundle; */
 /*     webKitBundle = [NSBundle bundleWithPath:@"/System/Library/Frameworks/WebKit.framework"]; */
 /*     if (webKitBundle) { */
