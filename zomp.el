321b442040ed75492c24fe5fab255409fa2ff21b
sheijk <sheijk@38bfec48-a720-0410-a12a-c49042e68019>
2010-09-05 20:49:47 +0000

    Do not highlight "x:" like a keyword if at end of line
    git-svn-id: http://subversion.dword.org/repos/sheijk/trunk/Stuff/ocaml/lang/v3@1192 38bfec48-a720-0410-a12a-c49042e68019

--- zomp.el
+++ zomp.el
@@ -764,7 +764,7 @@ editor to trigger recompilations etc. and possibly resume main()"
 
     ("// *\\(TODO\\)" 1 todo-face t t)
 
-    ("\\b[a-zA-Z0-9_]+\\b:" 0 font-lock-keyword-face)
+    ("\\(\\b[a-zA-Z0-9_]+\\b:\\) +[^ \n]" 1 font-lock-keyword-face)
     ("\\bconst\\b" 0 font-lock-keyword-face)
     ("\\bvar\\b" 0 font-lock-keyword-face)
     ("\\bfunc\\b" 0 font-lock-keyword-face)
