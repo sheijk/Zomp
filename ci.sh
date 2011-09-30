63a29a070a4365c3fc4518b5bee1fa72c2f45f90
Jan Rehders <cmdkeen@gmx.de>
2011-10-01 00:20:05 +0200

    ci script creates links to external tools

--- ci.sh
+++ ci.sh
@@ -3,8 +3,18 @@
 # A simple continuous integration script. Will do a git clone of the specified
 # directory, do a full build and run the unit test suite
 
-VC_DIR=$1
-EXTERNAL_TOOLS_DIR=$2
+if [ ! -d "$1" ]; then
+    echo "Cannot find vc dir '$1' in `pwd`"
+    exit 2
+fi
+
+if [ ! -d "$2" ]; then
+    echo "Cannot find external tools dir '$2' in `pwd`"
+    exit 3
+fi
+
+VC_DIR=`cd $1; pwd`
+EXTERNAL_TOOLS_DIR=`cd $2; pwd`
 FLAGS=$3
 
 # abort_on_error(action, command...)
@@ -13,25 +23,15 @@ function run_action {
     shift
     echo "running ${ACTION}: $@"
     LOGFILE=${TARGET_DIR}/log_${ACTION}.txt
-    $@ > ${LOGFILE}
-    EXITSTATUS=$?
-    cat ${LOGFILE}
+    $@ | tee ${LOGFILE}
+    EXITSTATUS=$PIPESTATUS
+    echo "Exited with code ${EXITSTATUS}" >> ${LOGFILE}
 
     if [ "$EXITSTATUS" -ne "0" ]; then
-        echo "${ACTION} failed with $EXITSTATUS"
+        echo "${ACTION} failed with ${EXITSTATUS}"
         exit 1
     fi
 }
-
-if [ ! -d "${VC_DIR}" ]; then
-    echo "Cannot find vc dir '${VC_DIR}' in `pwd`"
-    exit 2
-fi
-
-if [ ! -d "${EXTERNAL_TOOLS_DIR}" ]; then
-    echo "Cannot find external tools dir '${EXTERNAL_TOOLS_DIR}' in `pwd`"
-    exit 3
-fi
 
 echo "Starting build and test run"
 
@@ -39,9 +39,18 @@ TARGET_DIR_REL=ci_run_`date '+%Y-%m-%d_%H_%M_%S'`
 mkdir -p ${TARGET_DIR_REL}
 TARGET_DIR=`cd ${TARGET_DIR_REL}; pwd`
 echo "Target dir is ${TARGET_DIR}"
+echo "External tools dir is ${EXTERNAL_TOOLS_DIR}"
 
 run_action "git_clone" git clone ${VC_DIR} ${TARGET_DIR}/zomp_vc
-cd ${TARGET_DIR}/zomp_vc
+
+cd ${TARGET_DIR}
+cd zomp_vc
+
+# Setup links to external tools
+pwd
+mkdir -p tools
+ln -s ${EXTERNAL_TOOLS_DIR}/llvm-2.9 tools/llvm-2.9
+ln -s ${EXTERNAL_TOOLS_DIR}/llvm-gcc tools/llvm-gcc
 
 run_action "make_all" make ${FLAGS} all
 run_action "make_test" make ${FLAGS} test
