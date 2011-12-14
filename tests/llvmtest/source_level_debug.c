ad793669c76aebbbd5efdc65f5126708fdbe0206
Jan Rehders <cmdkeen@gmx.de>
2011-12-14 22:40:40 +0100

    experiments with LLVM debugging info

--- tests/llvmtest/source_level_debug.c
+++ tests/llvmtest/source_level_debug.c
@@ -5,11 +5,14 @@
 
 #include <stdio.h>
 
+int numA(int x) { return x + 1; }
+int numB(int x) { return x * 3; }
+
 int main(int argc, char* argv[])
 {
-    printf("foo\n");
-    printf("bar\n");
-    return 0;
+    int x = numA( 10 );
+    int y = numB( 20 );
+    return x + y;
 }
 
 
