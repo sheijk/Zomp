4de2731571a538bab1db2befdfcd493a3c94c912
Jan Rehders <cmdkeen@gmx.de>
2011-12-29 00:30:24 +0100

    improved readability of check_test

--- testsuite/check_test.ml
+++ testsuite/check_test.ml
@@ -17,56 +17,67 @@ let failWith error =
   flush stderr;
   exit errorCode
 
-let withOpenFileIn fileName f =
-  let stream = open_in fileName in
-  try
-    f stream;
-    close_in stream
-  with exn ->
-    close_in stream;
-    raise exn
-
-let withOpenFileOut fileName f =
-  let stream = open_out fileName in
-  try
-    f stream;
-    close_out stream
-  with exn ->
-    close_out stream;
-    raise exn
-
-let printFile filename =
-  let rec printStream s =
-    let line = input_line s in
-    print_string line;
-    print_newline();
-    printStream s
-  in
-  try
-    withOpenFileIn filename printStream
-  with End_of_file ->
-    ()
-
-let forEachLineInFile fileName f =
-  let rec read lineNum stream =
+module Utils =
+struct
+  let withOpenFileIn fileName f =
+    let stream = open_in fileName in
     try
-      let line = input_line stream in
-      let () = f lineNum line in
-      read (lineNum+1) stream
-    with End_of_file -> ()
-  in
-  withOpenFileIn fileName (read 1)
+      f stream;
+      close_in stream
+    with exn ->
+      close_in stream;
+      raise exn
 
-let replaceExtension fileName ext =
-  let noExt =
+  let withOpenFileOut fileName f =
+    let stream = open_out fileName in
     try
-      Filename.chop_extension fileName
-    with Invalid_argument _ ->
-      fileName
-  in
-  sprintf "%s.%s" noExt ext
+      f stream;
+      close_out stream
+    with exn ->
+      close_out stream;
+      raise exn
 
-let (=~) str re = Str.string_match (Str.regexp re) str 0
+  let printFile filename =
+    let rec printStream s =
+      let line = input_line s in
+      print_string line;
+      print_newline();
+      printStream s
+    in
+    try
+      withOpenFileIn filename printStream
+    with End_of_file ->
+      ()
+
+  let forEachLineInFile fileName f =
+    let rec read lineNum stream =
+      try
+        let line = input_line stream in
+        let () = f lineNum line in
+        read (lineNum+1) stream
+      with End_of_file -> ()
+    in
+    withOpenFileIn fileName (read 1)
+
+  let replaceExtension fileName ext =
+    let noExt =
+      try
+        Filename.chop_extension fileName
+      with Invalid_argument _ ->
+        fileName
+    in
+    sprintf "%s.%s" noExt ext
+
+  let safeParseInt str =
+    try
+      int_of_string str
+    with Failure _ ->
+      -1
+
+  let (=~) str re = Str.string_match (Str.regexp re) str 0
+end
+
+open Utils
 
 type expectation = ErrorMessage | WarningMessage | PrintMessage
 let expectationOfString str =
@@ -79,13 +90,8 @@ let expectationToString = function
   | WarningMessage -> "warning"
   | PrintMessage -> "print"
 
-let safeParseInt str =
-  try
-    int_of_string str
-  with Failure _ ->
-    -1
-
 let () =
+
   if false then begin
     printf "Called %s\n" (String.concat " " (Array.to_list Sys.argv));
     printf "From directory %s\n" (Sys.getcwd());
@@ -97,6 +103,7 @@ let () =
 
   let outputFileName = Sys.argv.(1) in
   let zompFileName = replaceExtension outputFileName "zomp" in
+
   let compilerMessagesOutputFile = Filename.temp_file "zompc" "out" in
   let compilerError =
     Sys.command (sprintf "( ./zompc.native -c %s 2>&1 ) > %s"
@@ -120,7 +127,6 @@ let () =
       printf "%s:%d: warning: invalid expectation kind '%s'. Expected error, warning or print\n"
         zompFileName lineNum kindStr
   in
-  
   let collectExpectations lineNum line =
     if line =~ ".*//// \\([^ ]+\\) \\(.*\\)" then begin
       let kind = Str.matched_group 1 line in
@@ -128,16 +134,29 @@ let () =
       addExpectation kind args lineNum
     end
   in
+
   forEachLineInFile zompFileName collectExpectations;
 
   let writeReport outFile =
-    List.iter (fun (kind, args, lineNum, found) ->
-                 fprintf outFile "Expecting %s at line %d containing words %s\n"
-                   (expectationToString kind)
-                   lineNum
-                   (String.concat ", " args);)
-      !expectedErrorMessages;
-    
+    let writeExpectation (kind, args, lineNum, found) =
+      fprintf outFile "Expecting %s at line %d containing words %s\n"
+        (expectationToString kind)
+        lineNum
+        (String.concat ", " args)
+    in
+    let checkExpectation message diagnosticLineNum (kind, args, expectedLineNum, found) =
+      if diagnosticLineNum = expectedLineNum then begin
+        let containsWord word =
+          let f = Str.string_match (Str.regexp (".*" ^ Str.quote word)) message 0 in
+          (if not f then
+              printf "Word '%s' missing in message '%s'\n" word message);
+          f
+        in
+        if List.for_all containsWord args then
+          found := true
+      end
+    in
+
     let errorRe = Str.regexp
       (sprintf "error: %s:\\([0-9]\\)+: .*error \\(.*\\)" (Str.quote zompFileName))
     in
@@ -148,40 +167,30 @@ let () =
       if isErrorMessage then begin
         let diagnosticLineNum = safeParseInt (Str.matched_group 1 line) in
         let message = Str.matched_group 2 line in
-        
-        let checkExpectation (kind, args, expectedLineNum, found) =
-          if diagnosticLineNum = expectedLineNum then begin
-            let containsWord word =
-              let f = Str.string_match (Str.regexp (".*" ^ Str.quote word)) message 0 in
-              (if not f then
-                 printf "Word '%s' missing in message '%s'\n" word message);
-              f
-            in
-            if List.for_all containsWord args then
-              found := true
-          end
-        in
-        List.iter checkExpectation !expectedErrorMessages
+        List.iter (checkExpectation message diagnosticLineNum) !expectedErrorMessages
       end
     in
-
-    fprintf outFile "Compiler output:\n";
-    forEachLineInFile compilerMessagesOutputFile checkExpectations;
 
     let reportMissingDiagnostic (kind, args, lineNum, found) =
       match kind with
         | ErrorMessage ->
-            if !found = false then
-              printf "%s:%d: failed to report error containing words %s\n"
-                zompFileName lineNum
-                (String.concat ", " args)
+          if !found = false then
+            printf "%s:%d: failed to report error containing words %s\n"
+              zompFileName lineNum
+              (String.concat ", " args)
         | WarningMessage ->
-            printf "%s:%d: warning: checking for warnings not supported, yet\n"
-              zompFileName lineNum
+          printf "%s:%d: warning: checking for warnings not supported, yet\n"
+            zompFileName lineNum
         | PrintMessage ->
-            printf "%s:%d: warning: checking for print not supported, yet\n"
-              zompFileName lineNum
+          printf "%s:%d: warning: checking for print not supported, yet\n"
+            zompFileName lineNum
     in
+
+    List.iter writeExpectation !expectedErrorMessages;
+    
+    fprintf outFile "Compiler output:\n";
+    forEachLineInFile compilerMessagesOutputFile checkExpectations;
+
     List.iter reportMissingDiagnostic !expectedErrorMessages;
   in
   withOpenFileOut outputFileName writeReport
