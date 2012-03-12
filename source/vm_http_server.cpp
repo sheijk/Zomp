28f45d97f25382af499602312360661c060f22c6
Jan Rehders <cmdkeen@gmx.de>
2012-03-12 02:43:34 +0100

    vm server links against zompvm_impl and llvm, initialized vm and provides simple display

--- source/vm_http_server.cpp
+++ source/vm_http_server.cpp
@@ -1,9 +1,13 @@
 #include "zomputils.h"
+#include "zompvm_impl.h"
 
 #include <stdio.h>
 #include <string.h>
 #include <map>
 #include <string>
+
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
 
 #include "mongoose.h"
 
@@ -15,16 +19,37 @@ static const char* htmlHeader =
 
 class HttpVMServer
 {
+    enum State {
+        StateNotInitialized,
+        StateRunning,
+        StateFailedToInitialize
+    };
+
+    State state_;
+    std::string stateDescr_;
+
 public:
+
+    HttpVMServer() {
+        state_ = StateNotInitialized;
+    }
 
     void run()
     {
-        struct mg_context *ctx;
-        const char *options[] = {"listening_ports", "8080", NULL};
+        if( !zompInit() ) {
+            state_ = StateFailedToInitialize;
+            stateDescr_ = "Failed to initialize Zomp VM";
+            return;
+        }
+        state_ = StateRunning;
 
-        ctx = mg_start(&callback, this, options);
-        getchar();  // Wait until user hits "enter"
+        const char *options[] = {"listening_ports", "8080", NULL};
+        mg_context* ctx = mg_start(&callback, this, options);
+        getchar(); // Wait until user hits "enter"
         mg_stop(ctx);
+
+        zompShutdown();
+        state_ = StateNotInitialized;
     }
 
 private:
@@ -39,6 +64,26 @@ private:
             "  <body>\n"
             "    <h1>Status</h1>\n"
             "    <p>running</p>\n"
+            "    <h2>LLVM</h2>\n");
+
+        llvm::LLVMContext* context = zompLLVMContext();
+        llvm::Module* module = zompLLVMModule();
+        std::string target = module->getTargetTriple();
+
+        mg_printf(conn, "  <p>Module identifier %s</p>\n", module->getModuleIdentifier().c_str());
+        mg_printf(conn, "  <p>Target %s</p>\n", target.c_str());
+        mg_printf(conn, "  <p>Data Layout %s</p>\n", module->getDataLayout().c_str());
+
+        mg_printf(conn, "  <h3>Functions</h3>\n");
+        llvm::Module::iterator function = module->begin();
+        llvm::Module::iterator functionEnd = module->end();
+        mg_printf(conn, "  <ul>\n");
+        for( ; function != functionEnd; ++function ) {
+            mg_printf( conn, "<li>%s</li>\n", function->getName().str().c_str() );
+        }
+        mg_printf(conn, "  </ul>\n");
+
+        mg_printf(conn,
             "  </body>\n"
             "</html>\n");
     }
