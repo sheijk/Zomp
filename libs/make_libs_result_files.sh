e6ba28e8d5c66994f6dd2de4c738f03d819cd1e5
Jan Rehders <cmdkeen@gmx.de>
2012-10-26 02:01:35 +0200

    Fixed broken generation of libs report.

--- libs/make_libs_result_files.sh
+++ libs/make_libs_result_files.sh
@@ -3,12 +3,12 @@
 # Creates .report files for the lib files in $1
 # 
 
-for src in $1; do
-	f=`basename $$src .zomp`;
-	(if [ -e libs/$$f.ll ]; then
+for src in $@; do
+	f=`basename $src .zomp`;
+	(if [ -e libs/$f.ll ]; then
 		echo "succeeded";
 	else
 		echo "compilation failed";
-	fi > libs/$$f.result)
+	fi > libs/$f.result)
 done
 
