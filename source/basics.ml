8855cc2022f721a5a02c6042c4601c999383c48d
Jan Rehders <cmdkeen@gmx.de>
2013-01-05 07:57:11 +0100

    added function

--- source/basics.ml
+++ source/basics.ml
@@ -11,6 +11,10 @@ type location = {
 
 let locationToString loc = sprintf "%s:%d" loc.fileName loc.line
 
+let locationEqual lhs rhs =
+  (lhs.line = rhs.line) &&
+    ((String.compare lhs.fileName rhs.fileName) = 0)
+
 let formatError location message =
   sprintf "%s: error: %s" (locationToString location) message
 
