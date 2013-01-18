613f2de76ca9b8e0923aa7c5a3c60b4f1bdb72a6
Jan Rehders <cmdkeen@gmx.de>
2013-01-18 23:23:02 +0100

    Added documentation and error check to test script.

--- test_vm_server.sh
+++ test_vm_server.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env sh
+#
+# A simple script to run integration test for zomp remote shell/vm.
+# Run this from the Zomp main dir.
+#
+# TODO: This might be outdated
+#
+
+if [ ! -e ./vm_server ]; then
+    echo "$0:1: error: missing ./vm_server. Make sure that you run this from the Zomp main dir and that ./vm_server has been built"
+    exit 1
+fi
 
 PORT=$((20000 + ${RANDOM} % 10000))
 
