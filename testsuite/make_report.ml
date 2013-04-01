99abee04851dd6b851aae0ff8890cc938189fd69
Jan Rehders <cmdkeen@gmx.de>
2013-04-01 23:09:59 +0200

    Grouping testsuite report by directory.

--- testsuite/make_report.ml
+++ testsuite/make_report.ml
@@ -11,27 +11,6 @@ let inHtmlTag tag args f =
   f();
   printf "</%s>\n" tag
 
-let deemphasizeCommonPrefix str1 str2 =
-  let maxLength = min (String.length str1) (String.length str2) in
-  let rec findFirstDifferentChar lastPathSepIndex index =
-    if index >= maxLength then
-      lastPathSepIndex
-    else
-      if str1.[index] = str2.[index] then
-        findFirstDifferentChar
-          (if str1.[index] = '/' then index + 1 else lastPathSepIndex)
-          (index+1)
-      else
-        lastPathSepIndex
-  in
-  let firstDifferentChar = findFirstDifferentChar 0 0 in
-  if (firstDifferentChar = 0) then
-    str2
-  else
-    let commonPrefix = Str.string_before str2 firstDifferentChar in
-    let str2Postfix = Str.string_after str2 firstDifferentChar in
-    sprintf "<span class=\"common-prefix\">%s</span>%s" commonPrefix str2Postfix
-
 let readFileIfExists fileName =
   try begin
     let ch = open_in fileName in
@@ -46,62 +25,105 @@ let readFileIfExists fileName =
   end with Sys_error _ ->
     ""
 
-let produceReport title files =
-  let succeededTests = ref 0 in
-  let prevFile = ref "" in
-  let testResultRows =
-    List.map
-      (fun fileName ->
-        let title = deemphasizeCommonPrefix !prevFile fileName in
-        let sourceLink = linkIfExists (sprintf "%s.zomp" fileName) title in
-        
-        let resultInfo =
-          let resultFileContent = readFileIfExists (fileName ^ ".result") in
-          if Sys.file_exists (fileName ^ ".result") then begin
-            let cssClass =
-              match resultFileContent with
-                | "failed" -> "failed"
-                | "failed!" -> "failed changed"
-                | "succeeded" ->
-                  incr succeededTests;
-                  "ok"
-                | "succeeded!" ->
-                  incr succeededTests;
-                  "ok changed"
-                | _ ->
-                  "failed"
-            in
-            sprintf "<th class=\"%s\">%s</th>" cssClass resultFileContent;
-          end else begin
-            sprintf "<th class=\"failed\">not run</th>"
-          end
-        in
-        
-        let reportLink = linkIfExists (fileName ^ ".testreport") "report"
-        and outputLink = linkIfExists (fileName ^ ".test_output") "output"
-        in
-        
-        prevFile := fileName;
-        
-        [sprintf "<th>%s</th>\n" sourceLink;
-         sprintf "%s\n" resultInfo;
-         sprintf "<th>%s</th>\n" reportLink;
-         sprintf "<th>%s</th>\n" outputLink])
-      files
+let groupBy compare pairs =
+  let sorted = List.sort (fun (a, _) (b, _) -> compare a b) pairs in
+  let lastKey, groups, lastGroupElements =
+    List.fold_left
+      (fun (currentKey, prevGroups, groupElements) (key, element) ->
+        if currentKey = key then
+          (currentKey, prevGroups, element :: groupElements)
+        else
+          (key, (currentKey, groupElements) :: prevGroups, [element]))
+      (fst (List.hd sorted), [], []) sorted
   in
-  
+  List.rev ((lastKey, lastGroupElements) :: groups)
+
+let groupFilesByDir files =
+  let dirsAndBasenames = List.map
+    (fun fileName ->
+      Filename.dirname fileName, Filename.basename fileName)
+    files
+  in
+  groupBy String.compare dirsAndBasenames
+
+let produceReport title files =
+  let groupedFiles = groupFilesByDir files in
+  let testResultRowsByDir =
+    List.map
+      (fun (dirname, files) ->
+        let succeededTests = ref 0 in
+        let reportRows = List.map
+          (fun fileBaseName ->
+            let fileName = Filename.concat dirname fileBaseName in
+            let sourceLink = linkIfExists (sprintf "%s.zomp" fileName) fileBaseName in
+
+            let resultInfo =
+              let resultFileContent = readFileIfExists (fileName ^ ".result") in
+              if Sys.file_exists (fileName ^ ".result") then begin
+                let cssClass =
+                  match resultFileContent with
+                    | "failed" -> "failed"
+                    | "failed!" -> "failed changed"
+                    | "succeeded" ->
+                      incr succeededTests;
+                      "ok"
+                    | "succeeded!" ->
+                      incr succeededTests;
+                      "ok changed"
+                    | _ ->
+                      "failed"
+                in
+                sprintf "<th class=\"%s\">%s</th>" cssClass resultFileContent;
+              end else begin
+                sprintf "<th class=\"failed\">not run</th>"
+              end
+            in
+
+            let reportLink = linkIfExists (fileName ^ ".testreport") "report"
+            and outputLink = linkIfExists (fileName ^ ".test_output") "output"
+            in
+
+            [sprintf "<th>&nbsp;&nbsp;&nbsp;&nbsp;%s</th>" sourceLink;
+             sprintf "%s" resultInfo;
+             sprintf "<th>%s</th>" reportLink;
+             sprintf "<th>%s</th>" outputLink])
+          files
+        in
+        dirname, reportRows, !succeededTests)
+      groupedFiles
+  in
+
   printf "<h1>%s</h1>\n" title;
-  print_string "<table class=\"test-results\">\n";
+  let totalTests = List.length files in
+  let succeededTests = List.fold_left
+    (fun totalSucceeded (_, _, succeeded) -> succeeded + totalSucceeded)
+    0
+    testResultRowsByDir
+  in
+  if (succeededTests = totalTests) then
+    printf "<p class=\"summary\">%d/%d succeeded</p>\n" succeededTests totalTests
+  else
+    printf "<p class=\"summary\"><span class=\"failed\">%d/%d</span> succeeded</p>\n" succeededTests totalTests;
+
   let printReportLine reportRows =
     print_string "  <tr>\n";
-    List.iter (printf "    %s") reportRows;
+    List.iter (printf "    %s\n") reportRows;
     print_string "  </tr>\n"
   in
-  List.iter printReportLine testResultRows;
-  print_string "</table>\n";
-  
-  let totalTests = List.length files in
-  printf "<p>%d/%d succeeded</p>\n" !succeededTests totalTests
+  print_string "<table class=\"test-results\">\n";
+  List.iter
+    (fun (basedir, testResultRows, succeeded) ->
+      let styledBaseDir = sprintf "<span class=\"dirname\">%s</span>" basedir in
+      let styledSummary =
+        let total = List.length testResultRows in
+        let css = if succeeded = total then "short-summary" else "failed" in
+        sprintf "<span class=\"%s\">(%d/%d)</span>" css succeeded total
+      in
+      printf "  <tr><th>%s %s</th><th></th><th></th><th></th></tr>\n"
+        styledBaseDir styledSummary;
+      List.iter printReportLine testResultRows)
+    testResultRowsByDir;
+  print_string "</table>\n"
     
 let () =
   match Array.to_list Sys.argv with
