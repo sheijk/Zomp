53ea27ed7c31e96f69faf1d10082f205f730b507
Jan Rehders <cmdkeen@gmx.de>
2013-05-10 20:43:04 +0200

    Small documentation update.

--- source/indentlexer.mli
+++ source/indentlexer.mli
@@ -1,10 +1,15 @@
 exception Eof
 type token = Newparser.token
 type 'a lexerstate
-val readChar : 'a lexerstate -> char
+
+(** Found a character sequence that is not a valid token. *)
 exception UnknowToken of Basics.location * string * string
 val unknownTokenToErrorMsg : Basics.location option * string * string -> string
+
+(** Found invalid indentation *)
 exception IndentError of Basics.location * string
+
+(** Read the next token *)
 val token : Newparser.token lexerstate -> Newparser.token
 
 val lexbufFromChannel : string -> in_channel -> 'a lexerstate
@@ -13,6 +18,9 @@ val lexString : string -> Newparser.token list
 
 val tokensToString : Newparser.token list -> string
 val printTokens : Newparser.token list -> unit
+
+(** For testing, only. Returns None if at end of input. *)
+val readChar : 'a lexerstate -> char
 
 val runInternalTests : unit -> unit
 val printStats : unit -> unit
