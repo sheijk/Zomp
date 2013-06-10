92f465b902b9b55572d07623ab0668681a984fc6
Jan Rehders <cmdkeen@gmx.de>
2013-06-10 03:49:43 +0200

    New util function.

--- source/common.ml
+++ source/common.ml
@@ -253,6 +253,13 @@ struct
   let withFileForWriting filename f =
     (makeGuardedFunction open_out close_out) filename f
 
+  let withFileForAppending filename f =
+    (makeGuardedFunction
+       (open_out_gen [Open_wronly; Open_append; Open_creat; Open_text] 0o666)
+       close_out)
+      filename
+      f
+
   let absolutePath fileName =
     let fileName =
       if fileName = "." then ""
