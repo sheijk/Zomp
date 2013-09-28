8cb6e9984d9c4c1a7490cb1b4592e0c31db4c4cb
Jan Rehders <cmdkeen@gmx.de>
2013-09-28 02:50:57 +0200

    grep helper uses modern regexp syntax.

--- grep.sh
+++ grep.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-find . \( -iname .git -or -iname data -or -iname tools \) -prune -or -type f -exec grep -nH $* {} +
+find . \( -iname .git -or -iname data -or -iname tools \) -prune -or -type f -exec grep -E -nH $* {} +
 
