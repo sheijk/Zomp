e14d6c5756d3bcfb621e26d0697f3b03e014e468
Jan Rehders <cmdkeen@gmx.de>
2013-10-04 03:55:57 +0200

    build.sh script supports --clean.

--- build.sh
+++ build.sh
@@ -7,6 +7,14 @@
 BUILDLOG=build/buildlog.txt
 BUILDLOG_REPORT=build/buildlog_report.txt
 
+CLEAN_BUILD=0
+
+case "$1" in
+    "--clean")
+        shift
+        CLEAN_BUILD=1 ;;
+esac
+
 # Find all make options of the form FOO_BAR=... to keep them for multiple make
 # invocations
 OPTIONS=`echo "$@" | tr " " "\n" | grep "^[A-Za-z_][A-Za-z0-9_]*=" | tr "\n" " "`
@@ -19,6 +27,13 @@ echo "Starting build at `date '+%Y-%m-%d %H:%M:%S'` with params '$@', options = 
 # Add --print-directory even though we're not using recursive make so Emacs (and
 # other tools) shows correct source locations even when compilation command has
 # not been triggered from this directory.
+
+if [ "${CLEAN_BUILD}" -eq "1" ]
+then
+    echo "Cleaning before build ..."
+    make --print-directory -s ${OPTIONS} clean || exit 1
+fi
+
 make --print-directory $@ SHELL="${LOGSHELL} ${BUILDLOG}"
 RETVAL=$?
 
