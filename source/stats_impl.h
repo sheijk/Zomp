b9caa60b91a351469a65d70e19689a44e3b5b639
Jan Rehders <cmdkeen@gmx.de>
2013-10-07 04:08:09 +0200

    Adding documentation.

--- source/stats_impl.h
+++ source/stats_impl.h
@@ -7,29 +7,61 @@
 extern "C" {
 #endif
 
+/// A counter
 struct Counter;
+
+/// Sections form the counter hierarchy. They can contain other sections and
+/// counters and can have a name.
 struct Section;
 
+/// Function type used to query the value of a counter.
 typedef i64 (*CounterQueryFunction)(Counter*, void*);
-    
+
+/// The root section. Has no name and does not contain any counters.
 Section* statsMainSection();
+
+/// Adds a new child section. Does not check if another one of the same name
+/// already exists.
 Section* statsCreateSection(Section* parent, const char* name);
-// void statsDeleteSection(Section*);
+
+/// Add a counter to the given section. The query function will be called when
+/// @statsPrintReport gets called. userData will be passed to it.
 Counter* statsCreateCounter(Section* parent, const char* name, u32 fractionalDigits, void* userData, CounterQueryFunction query);
+
+/// Creates a counter whose value is read by de-referencing ptr.
 Counter* statsCreateCounterForValue(Section* parent, const char* name, u32 fractionalDigits, int* ptr);
+
+/// Will add a section with the given name to the root section. Will not add
+/// a second one of one already exists with the given name.
 void statsCreateNamedSection(const char* sectionName);
+
+/// Will create a counter whose value is read by passing the given id to the
+/// OCaml function denoted by the named value getCounterValue.
 void statsCreateCamlCounter(const char* sectionName, const char* name, int id, int fractionalDigits);
+
+// TODO: add functions to freeze counters/sections (so their query functions)
+// won't get called, again.
+// void statsDeleteSection(Section*);
 // void statsDeleteCounter(Counter*);
 
+/// Pointer to first sub section. NULL if section does not have sub sections.
 Section* statsFirstChildSection(Section*);
+/// The child following the given section in it's parent or NULL if it is the last.
 Section* statsNextSection(Section*);
+
+/// Pointer to first counter. NULL if section does not have any counters;
 Counter* statsFirstCounter(Section*);
+/// The counter following the given counter in it's section or NULL if it is the last.
 Counter* statsNextCounter(Counter*);
 
+/// A zero terminated string. Will never be NULL.
 const char* statsCounterName(Counter*);
+/// Calls the query function of the counter and returns the value it returned.
 i64 statsCounterQuery(Counter*);
+/// The number of fractional digits used for displaying.
 i32 statsCounterFractionalDigits(Counter*);
 
+/// Will print the values of all counters to stdout.
 void statsPrintReport(int indent);
 
 #ifdef __cplusplus
