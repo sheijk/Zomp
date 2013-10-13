sheijk/Zomp

commit 478bcc81b39a46a280c7ce7bdcfe3eb3bcc7915f

Keep zompc compiler output for all files.
So they can be included and linked in test reports for libs and examples.

makefile
@@ -317,11 +317,10 @@ test: $(TEST_SUB_TARGETS)
 	@$(ECHO) "Compiling $< ..."
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
-
 .PRECIOUS: %.ll %.bc %.opt-bc
-%.ll %.compile_output: %.zomp $(ZOMPC_FILE) $(OUT_DIR)/has_llvm
+%.ll: %.zomp $(ZOMPC_FILE) $(OUT_DIR)/has_llvm
 	$(ECHO) Compiling $(<) to .ll...
-	($(ZOMPC) -c $< $(ZOMPCFLAGS) 2>&1 | tee $(<:.zomp=.compile_output)) || (rm -f $@; exit 1)
+	$(ZOMPC) -c $< $(ZOMPCFLAGS) || (rm -f $@; exit 1)
 
 %.bc: %.ll $(OUT_DIR)/has_llvm
 	@echo Compiling $< to $@

commit eb838c64a8ccde509f6fa26d5d9abe52a0798921

Re-enable catching of Zomp compiler output.
Was broken after revision 478bcc81 accidentally reverted this.

makefile
@@ -439,9 +439,9 @@ endif
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
 
 .PRECIOUS: %.ll %.bc %.opt-bc
-%.ll: %.zomp $(ZOMPC_FILE) $(OUT_DIR)/has_llvm
+%.ll %.compile_output: %.zomp $(ZOMPC_FILE) $(OUT_DIR)/has_llvm
 	$(ECHO) Compiling $(<) to .ll...
-	$(ZOMPC) -c $< $(ZOMPCFLAGS) || (rm -f $@; exit 1)
+	($(ZOMPC) -c $< $(ZOMPCFLAGS) | tee $(<:.zomp=.compile_output)) || (rm -f $@; exit 1)
 
 %.bc: %.ll $(OUT_DIR)/has_llvm
 	@echo Compiling $< to $@