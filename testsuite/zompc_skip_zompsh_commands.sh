d0487563f172a54140e94d862a47b0bd94c47ba3
Jan Rehders <cmdkeen@gmx.de>
2014-01-09 01:05:27 +0100

    Fixing file names for testsuite/zompsh/..
    When compiling them using zompc they get filtered to get rid of zompsh
    commands and written to a new file. Now fixing the file names in
    reported errors.

--- testsuite/zompc_skip_zompsh_commands.sh
+++ testsuite/zompc_skip_zompsh_commands.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # Will remove all lines that are zompsh commands (starting with !) and then run
-# the result through zomc
+# the result through zompc
 
 ZOMPC=$1
 if [[ "$2" != "-c" ]] || [ -z ${3} ]; then
@@ -10,11 +10,13 @@ fi
 FILE_NAME=$3
 shift 3
 
+FIX_FILE_NAME="sed s/no_zompsh_test_/test_/g"
 TEMP_FILE=${FILE_NAME/test_/no_zompsh_test_}
 cat ${FILE_NAME} | sed -E 's#^(!.*)#// (removed) \1#' > ${TEMP_FILE}
 
 echo "running ${ZOMPC} -c ${TEMP_FILE} $*"
-${ZOMPC} -c ${TEMP_FILE} $*
+# filtering stderr through grep
+(${ZOMPC} -c ${TEMP_FILE} $* | ${FIX_FILE_NAME}) 2> >(${FIX_FILE_NAME} 1>&2)
 EXIT_CODE=$?
 mv ${TEMP_FILE%.zomp}.ll ${FILE_NAME%.zomp}.ll
 # rm -f ${TEMP_FILE}
