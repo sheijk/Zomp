d8288079437298deccfac4d8c450fa0f9f5c7876
Jan Rehders <cmdkeen@gmx.de>
2014-02-25 00:24:49 +0100

    Cleanup interface.

--- source/expander.mli
+++ source/expander.mli
@@ -5,16 +5,6 @@
 *)
 exception IllegalExpression of Ast2.sexpr * Serror.t list
 
-module Mayfail : sig
-  type 'a mayfail = private Result of 'a | Error of Serror.t list
-
-  val errorFromString : Basics.location -> string -> 'a mayfail
-  val errorFromExpr : Ast2.sexpr -> string -> 'a mayfail
-  val singleError : Serror.t -> 'a mayfail
-  val multipleErrors : Serror.t list -> 'a mayfail
-  val result : 'a -> 'a mayfail
-end
-
 type tlenv
 val createEnv : Bindings.t -> tlenv
 val compileExpr : tlenv -> Ast2.t -> Lang.toplevelExpr Result.t * string
@@ -27,8 +17,6 @@ val emitError : tlenv -> Serror.t -> unit
 
 val addDllPath : tlenv -> string -> [`Front | `Back] -> unit
 val addIncludePath : tlenv -> string -> [`Front | `Back] -> unit
-
-type toplevelTranslationResult = (Bindings.t * Lang.toplevelExpr list) Mayfail.mayfail
 
 val addTranslateFunction : string -> doc:string -> (tlenv -> Ast2.sexpr -> unit) -> unit
 
