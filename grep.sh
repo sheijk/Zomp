ec4ec71f3bc2127ce6c8f81744db91de0e92fa9e
Jan Rehders <cmdkeen@gmx.de>
2014-09-15 08:38:24 +0200

    Fix whitespace handling in script.

--- grep.sh
+++ grep.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 BASE_DIR=`dirname $0`
-find ${BASE_DIR} \( -iname .git -or -iname data -or -iname tools -or -iname build \) -prune -or -type f -exec grep -E -nH $* {} +
+find ${BASE_DIR} \( -iname .git -or -iname data -or -iname tools -or -iname build \) -prune -or -type f -exec grep -E -nH "$@" {} +
 
