3b3007afe5ad3c23b41d41da1306fbce4866b1d5
Jan Rehders <cmdkeen@gmx.de>
2014-10-02 04:01:00 +0200

    Removed grep script, use git grep instead.

--- grep.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-BASE_DIR=`dirname $0`
-find ${BASE_DIR} \( -iname .git -or -ipath ${BASE_DIR}/ci_archive -or -ipath ${BASE_DIR}/data -or -ipath ${BASE_DIR}/tools -or -ipath ${BASE_DIR}/build \) -prune -or -type f -exec grep -E -nH "$@" {} +
-
